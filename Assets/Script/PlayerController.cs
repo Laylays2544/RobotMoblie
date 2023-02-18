d8297efbace8c66c338bda14dd7c09ae947481d2
Laylays2544 <amklin_k@silpakorn.edu>
2023-02-18 08:17:49 +0700

    DashRobot

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public float dashSpeed;
+    public float dashDuration;
+    public float dashCooldown;
 
     private Rigidbody2D myRigidbody;
 
@@ -15,15 +18,17 @@ public class PlayerController : MonoBehaviour
     private Collider2D myCollider;
 
     private bool canDoubleJump; // เพิ่มตัวแปรสำหรับเช็ค DoubleJump
+    private bool isDashing;
+    private float dashTime;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-
         myCollider = GetComponent<Collider2D>();
-
         canDoubleJump = true; // กำหนดค่าเริ่มต้นสำหรับ DoubleJump
+        isDashing = false;
+        dashTime = 0f;
     }
 
     // Update is called once per frame
@@ -32,20 +37,44 @@ public class PlayerController : MonoBehaviour
 
         grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
 
-        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        if (!isDashing)
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             if (grounded)
             {
                 myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-                canDoubleJump = true; // เมื่อกระโดดครั้งแรกกำหนดให้สามารถ DoubleJump ได้
+                canDoubleJump = true;
             }
-            else if (canDoubleJump) // เช็คสำหรับ DoubleJump
+            else if (canDoubleJump)
             {
                 myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-                canDoubleJump = false; // เมื่อ DoubleJump แล้วกำหนดให้ไม่สามารถ DoubleJump ได้อีก
+                canDoubleJump = false;
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !isDashing && dashTime <= 0)
+        {
+            isDashing = true;
+            dashTime = dashDuration;
+            myRigidbody.velocity = new Vector2(dashSpeed * Mathf.Sign(myRigidbody.velocity.x), myRigidbody.velocity.y);
+        }
+
+        if (isDashing)
+        {
+            dashTime -= Time.deltaTime;
+            if (dashTime <= 0)
+            {
+                isDashing = false;
+            }
+        }
+
+        if (dashTime <= 0)
+        {
+            dashTime = 0;
         }
     }
 }
