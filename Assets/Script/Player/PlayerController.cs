fad57d560c772db7c04da904d9218ef910881d5b
Laylays2544 <amklin_k@silpakorn.edu>
2023-02-24 18:59:08 +0700

    Part9

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     public float dashDuration;
     public float dashCooldown;
 
+    public float jumpTime;
+    private float jumpTimeCounter;
+
     private Rigidbody2D myRigidbody;
 
     public bool grounded;
@@ -17,7 +20,6 @@ public class PlayerController : MonoBehaviour
 
     private Collider2D myCollider;
 
-    private bool canDoubleJump; // เพิ่มตัวแปรสำหรับเช็ค DoubleJump
     private bool isDashing;
     private float dashTime;
 
@@ -26,23 +28,28 @@ public class PlayerController : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<Collider2D>();
-        canDoubleJump = true; // กำหนดค่าเริ่มต้นสำหรับ DoubleJump
+        jumpTimeCounter = jumpTime;
         isDashing = false;
         dashTime = 0f;
     }
     public void Jump()
     {
-        Debug.Log("Jumped");
         if (grounded)
         {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-            canDoubleJump = true;
         }
-        else if (canDoubleJump)
+
+        if(jumpTimeCounter > 0)
         {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-            canDoubleJump = false;
-            Debug.Log("Double Jumped");
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+         jumpTimeCounter = 0;
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
         }
     }
 
@@ -116,44 +123,4 @@ public class PlayerController : MonoBehaviour
         //    dashTime = 0;
         //}
     }
-    //public void Jump()
-    //{
-    //    Debug.Log("Jumped");
-    //    if (grounded)
-    //    {
-    //        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-    //        canDoubleJump = true;
-    //    }
-    //    else if (canDoubleJump)
-    //    {
-    //        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-    //        canDoubleJump = false;
-    //        Debug.Log("Double Jumped");
-    //    }
-    //}
-
-    //public void Dash()
-    //{
-    //    if (!isDashing && dashTime <= 0)
-    //    {
-    //        isDashing = true;
-    //        dashTime = dashDuration;
-    //        myRigidbody.velocity = new Vector2(dashSpeed * Mathf.Sign(myRigidbody.velocity.x), myRigidbody.velocity.y);
-    //        Debug.Log("Dashed");
-    //    }
-    //    if (isDashing)
-    //    {
-    //        dashTime -= Time.deltaTime;
-    //        if (dashTime <= 0)
-    //        {
-    //            isDashing = false;
-    //            Debug.Log("Dash Ended");
-    //        }
-    //    }
-
-    //    if (dashTime <= 0)
-    //    {
-    //        dashTime = 0;
-    //    }
-    //}
 }
