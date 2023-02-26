d893a46c62d9231dac54979ca4fe6e5049d78e84
Laylays2544 <amklin_k@silpakorn.edu>
2023-02-26 16:40:11 +0700

    SpeedUp

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public float speedMultiplier;
+
+    public float speedIncreaseMilestone;
+    private float speedMilestoneCount;
+
     public float jumpForce;
-    public float dashSpeed;
-    public float dashDuration;
-    public float dashCooldown;
+
+    //public float dashSpeed;
+    //public float dashDuration;
+    //public float dashCooldown;
+
+    //private bool isDashing;
+    //private float dashTime;
 
     public float jumpTime;
     private float jumpTimeCounter;
@@ -17,11 +26,10 @@ public class PlayerController : MonoBehaviour
 
     public bool grounded;
     public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
 
     private Collider2D myCollider;
-
-    private bool isDashing;
-    private float dashTime;
 
     // Start is called before the first frame update
     void Start()
@@ -29,64 +37,29 @@ public class PlayerController : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<Collider2D>();
         jumpTimeCounter = jumpTime;
-        isDashing = false;
-        dashTime = 0f;
+        speedMilestoneCount = speedIncreaseMilestone;
+        //isDashing = false;
+        //dashTime = 0f;
     }
-    public void Jump()
-    {
-        if (grounded)
-        {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-        }
-
-        if(jumpTimeCounter > 0)
-        {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-            jumpTimeCounter -= Time.deltaTime;
-        }
-
-         jumpTimeCounter = 0;
-
-        if (grounded)
-        {
-            jumpTimeCounter = jumpTime;
-        }
-    }
-
-    public void Dash()
-    {
-        if (!isDashing && dashTime <= 0)
-        {
-            isDashing = true;
-            dashTime = dashDuration;
-            myRigidbody.velocity = new Vector2(dashSpeed * Mathf.Sign(myRigidbody.velocity.x), myRigidbody.velocity.y);
-            Debug.Log("Dashed");
-        }
-        if (isDashing)
-        {
-            dashTime -= Time.deltaTime;
-            if (dashTime <= 0)
-            {
-                isDashing = false;
-                Debug.Log("Dash Ended");
-            }
-        }
-
-        if (dashTime <= 0)
-        {
-            dashTime = 0;
-        }
-    }
+    
     // Update is called once per frame
     void Update()
     {
 
         grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
 
-        if (!isDashing)
+        //grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if(transform.position.x > speedMilestoneCount)
         {
-            myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+            speedMilestoneCount += speedIncreaseMilestone;
+
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
+            moveSpeed = moveSpeed + speedMultiplier;
         }
+
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        
 
         //if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         //{
@@ -101,20 +74,45 @@ public class PlayerController : MonoBehaviour
         //        canDoubleJump = false;
         //    }
         //}
+    }
 
-        //if (Input.GetKeyDown(KeyCode.Q) && !isDashing && dashTime <= 0)
+    public void Jump()
+    {
+        if (grounded)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+
+        if (jumpTimeCounter > 0)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        jumpTimeCounter = 0;
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+    }
+
+    public void Dash()
+    {
+        //if (!isDashing && dashTime <= 0)
         //{
         //    isDashing = true;
         //    dashTime = dashDuration;
         //    myRigidbody.velocity = new Vector2(dashSpeed * Mathf.Sign(myRigidbody.velocity.x), myRigidbody.velocity.y);
+        //    Debug.Log("Dashed");
         //}
-
         //if (isDashing)
         //{
         //    dashTime -= Time.deltaTime;
         //    if (dashTime <= 0)
         //    {
         //        isDashing = false;
+        //        Debug.Log("Dash Ended");
         //    }
         //}
 
